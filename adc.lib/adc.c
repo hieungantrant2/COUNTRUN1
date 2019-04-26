bbee441b8813bde054e0deef065ab6051fcf49ec
hieungantrant2 <hieungantrant2@gmail.com>
2019-04-26 23:10:55 +0700

    ngan chance port input for adc.c p1.1 -> p1.0

--- adc.lib/adc.c
+++ adc.lib/adc.c
@@ -7,7 +7,7 @@
 void adc_init(void)
 {
     ADC10CTL0 = ADC10SHT_1 + ADC10ON + ADC10IE; //
-    ADC10CTL1 = INCH_1+ ADC10SSEL_3 ;
+    ADC10CTL1 = INCH_0+ ADC10SSEL_3 ;
     ADC10AE0 |= BIT1; //p1.1
 }
 int adc_tranfer(void)
